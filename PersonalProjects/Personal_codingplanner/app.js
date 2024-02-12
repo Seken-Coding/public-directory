3971c4d7c0f27ab03f8349d088ddce4d3af6c0c4
Alex Chong <alex.chong@outlook.de>
2024-02-12 09:16:25 +0100

    Update February
    Finishing up HTML course
    Working on some personal project

---
+++ PersonalProjects/Personal_codingplanner/app.js
@@ -0,0 +1,24 @@
+function createStart(params) {
+    //creates a start
+}
+
+function basicPath(params) {
+    //basic 1 way path
+}
+
+function splitPath(params) {
+    //adv 2 way path
+}
+
+function removeElement(params) {
+    //usable remove button
+}
+
+function reset(params) {
+    //resets the whole tree
+}
+
+function savePlan(params) {
+    //saves whole plan in either txt or picture
+}
+
