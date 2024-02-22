33a6014c08e8edb30ebd4f6be0c7b10e8b05a668
Alex Chong <alex.chong@outlook.de>
2024-02-22 20:20:04 +0100

    More notes on my personal cheatsheet

--- PersonalProjects/TestEnvCSharp/Program.cs
+++ PersonalProjects/TestEnvCSharp/Program.cs
@@ -1,1 +1,13 @@
 ﻿
+    public class MyClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Greeting("Hello"));
+        }
+        static string Greeting(string testString)
+        {
+            return "Good day to you";
+        }
+    }
+
