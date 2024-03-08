f2349d1dcb7126534f56b75813feec142b7f02fe
Alex Chong <alex.chong@outlook.de>
2024-03-08 11:03:55 +0100

    Sorting and managing

--- PersonalProjects/TestEnvCSharp/obj/Debug/net8.0/TestEnvCSharp.AssemblyInfo.cs
+++ PersonalProjects/TestEnvCSharp/obj/Debug/net8.0/TestEnvCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestEnvCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+531cff154da8afd0f3839490f6eb13974bd6867d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a931dedf1b97dedb83cf77556f5d6ffa94f5582")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestEnvCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestEnvCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
